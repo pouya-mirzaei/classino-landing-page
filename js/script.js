c0fe6f3130df676c23eded8adb702c2cfbd5d295
pouya <pouya.hp2004@yahoo.com>
2023-08-06 20:59:21 +0330

    feat : added the landing slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const swiperTop = new Swiper(".slider-top", {
   loop: true,
 
   navigation: {
@@ -8,6 +8,21 @@ const swiper = new Swiper(".swiper", {
 
   autoplay: {
     delay: 4000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
+
+const swiperLanding = new Swiper(".slider-landing", {
+  loop: true,
+
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+
+  autoplay: {
+    delay: 2000,
     disableOnInteraction: false,
     pauseOnMouseEnter: true,
   },
